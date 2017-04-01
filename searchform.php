ea9d879636af6e99a4d6823fb6c610bf8acf32ba
a.chigozie <shesgottadevelopit@gmail.com>
2017-03-31 23:32:43 -0400

    updated archform nd normalize file

--- searchform.php
+++ searchform.php
@@ -1,14 +1,15 @@
 <?php
 /**
  * Searchform template file
- * @package lemonade
+ * @package project52weeks
  */
 ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'sgdi' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'lemonade' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'project52weeks' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'lemonade' ); ?></span></button>
+	<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+<!--	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php //echo _x( 'Search', 'submit button', 'project52weeks' ); ?></span></button>-->
 </form>
