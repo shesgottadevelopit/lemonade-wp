757555cd095035296cbc8afe143e2f2f910cc721
a.chigozie <shesgottadevelopit@gmail.com>
2017-05-02 17:15:16 -0400

    updated theme names throughout and etc

--- searchform.php
+++ searchform.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * Searchform template file
- * @package project52weeks
+ * @package lemonade
+ * @link https://developer.wordpress.org/reference/functions/get_search_form/
  */
 ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'sgdi' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'project52weeks' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'lemonade' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'lemonade' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
 	<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-<!--	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php //echo _x( 'Search', 'submit button', 'project52weeks' ); ?></span></button>-->
+<!--	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php //echo _x( 'Search', 'submit button', 'lemonade' ); ?></span></button>-->
 </form>
